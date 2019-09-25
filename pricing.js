9ffa2bed680c94b87e509444235066a4ab553d43
Sean Biller <seanmbiller@gmail.com>
2019-09-24 22:10:04 -0400

    Added Calculate VolLife Function

--- pricing.js
+++ pricing.js
@@ -1,23 +1,27 @@
+function calculateVolLife (coverageLevels, price, product) {
+  for (var i = 0; i < coverageLevels.length; i++) {
+    var coverageAmount = coverageLevels[i].coverage
+
+    price += (coverageAmount / product.cost.costDivisor) * product.cost.price
+}
+
+if (product.employerContribution.mode === 'dollar') {
+  price = price - product.employerContribution.contribution
+} else {
+  dollarsOff = price * (product.employerContribution.contribution / 100)
+  price = price - dollarsOff
+}
+
+  return parseInt(price * 100) / 100
+}
+
 module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
   var price = 0
   var dollarsOff = 0
 
   switch (product.type) {
     case 'volLife':
-      for (var i = 0; i < coverageLevels.length; i++) {
-        var coverageAmount = coverageLevels[i].coverage
-
-        price += (coverageAmount / product.cost.costDivisor) * product.cost.price
-      }
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * (product.employerContribution.contribution / 100)
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
+      return calculateVolLife(coverageLevels, price, product)
     case 'ltd':
       var salaryPercentage = product.coveragePercentage / 100
 
