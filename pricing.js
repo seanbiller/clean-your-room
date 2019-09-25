67db181fc8a8c38acdbaee79acbc2caafb678389
Sean Biller <seanmbiller@gmail.com>
2019-09-24 22:31:36 -0400

    Added Calculate LTD

--- pricing.js
+++ pricing.js
@@ -14,6 +14,21 @@ if (product.employerContribution.mode === 'dollar') {
 
   return parseInt(price * 100) / 100
 }
+function calculateLTD (product,price, employee)  {
+  {  
+  var salaryPercentage = product.coveragePercentage / 100 
+
+    price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
+  }
+if (product.employerContribution.mode === 'dollar') {
+  price = price - product.employerContribution.contribution
+} else {
+  dollarsOff = price * product.employerContribution.contribution
+  price = price - dollarsOff
+}
+
+return parseInt(price * 100) / 100
+}
 
 module.exports.calculateProductPrice = function (product, employee, coverageLevels) {
   var price = 0
@@ -23,19 +38,9 @@ module.exports.calculateProductPrice = function (product, employee, coverageLeve
     case 'volLife':
       return calculateVolLife(coverageLevels, price, product)
     case 'ltd':
-      var salaryPercentage = product.coveragePercentage / 100
-
-      price += ((employee.salary * salaryPercentage) / product.cost.costDivisor) * product.cost.price
-
-      if (product.employerContribution.mode === 'dollar') {
-        price = price - product.employerContribution.contribution
-      } else {
-        dollarsOff = price * product.employerContribution.contribution
-        price = price - dollarsOff
-      }
-
-      return parseInt(price * 100) / 100
-    default:
-      return 0
+      return calculateLTD (product, price, employee)
+      
+    default: 
+  return 0
   }
 }
